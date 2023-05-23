93953c750c6bba5b7a95108e11837a31b8b1bfb4
Aymen <Aymen@MacBook-Air.local>
2023-05-23 23:32:37 +0100

    MAIN SLIDER

--- components/MainSlider.tsx
+++ components/MainSlider.tsx
@@ -1,5 +1,6 @@
 "use client";
 // Import Swiper React components
+import { FaChevronCircleRight } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -7,7 +8,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
-import { Pagination } from "swiper";
+import { Autoplay, Pagination } from "swiper";
 
 const MainSlider = () => {
   return (
@@ -17,18 +18,74 @@ const MainSlider = () => {
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
-        className="mySwiper w-full h-[25rem]"
+        // TODO: autoplay={{
+        // TODO  delay: 3500,
+        // TODO  disableOnInteraction: false,
+        // TODO }}
+        loop={true}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper w-full md:h-[24rem] h-[28rem]"
       >
-        <SwiperSlide className="bg-[url('../public/assets/bg-1.jpeg')] w-[90%] h-[90%] bg-white text-black p-16">
-          <h1>GAME OF THE WEEK</h1>
-          <p>
-            Join the party and hit the jackpot today at Funky Coins - where
-            every bet is a chance to turn up the volume on your winnings!
-          </p>
+        <SwiperSlide className="bg-one bg-cover bg-center bg-no-repeat w-full h-full text-white md:px-16 px-6 rounded-3xl s-1 border border-white/50 !flex justify-start items-center ">
+          <div className="flex flex-col gap-4 md:justify-start md:w-[55%] w-full justify-center sm:text-left text-center">
+            <h1 className="lg:text-5xl md:text-4xl text-3xl font-bold">
+              GAME OF THE WEEK
+            </h1>
+            <p className="md:text-sm text-xs">
+              Join the party and hit the jackpot today at Funky Coins - where{" "}
+              every bet is a chance to turn up the volume on your winnings!
+            </p>
+            <button className="gap-3 rounded-lg group uppercase px-3 py-2 bg-primary flex items-center justify-center md:justify-start md:text-sm text-xs font-medium sm:w-fit">
+              <p className="uppercase md:text-sm text-xs font-medium">
+                connect wallet
+              </p>
+              <FaChevronCircleRight
+                size={18}
+                className="group-hover:ml-2 transition-all text-black bg-white rounded-full"
+              />
+            </button>
+          </div>
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide className="bg-two bg-cover bg-center bg-no-repeat w-full h-full text-white md:px-16 px-6 rounded-3xl s-1 border border-white/50 !flex justify-start items-center ">
+          <div className="flex flex-col gap-4 md:justify-start md:w-[55%] w-full justify-center sm:text-left text-center">
+            <h1 className="lg:text-5xl md:text-4xl text-3xl font-bold">
+              GAME OF THE WEEK
+            </h1>
+            <p className="md:text-sm text-xs">
+              Join the party and hit the jackpot today at Funky Coins - where{" "}
+              every bet is a chance to turn up the volume on your winnings!
+            </p>
+            <button className="gap-3 rounded-lg group uppercase px-3 py-2 bg-primary flex items-center justify-center md:justify-start md:text-sm text-xs font-medium sm:w-fit">
+              <p className="uppercase md:text-sm text-xs font-medium">
+                connect wallet
+              </p>
+              <FaChevronCircleRight
+                size={18}
+                className="group-hover:ml-2 transition-all text-black bg-white rounded-full"
+              />
+            </button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="bg-three bg-cover bg-center bg-no-repeat  w-full h-full text-white md:px-16 px-6 rounded-3xl s-1 border border-white/50 !flex justify-start items-center ">
+          <div className="flex flex-col gap-4 md:justify-start md:w-[55%] w-full justify-center sm:text-left text-center">
+            <h1 className="lg:text-5xl md:text-4xl text-3xl font-bold">
+              GAME OF THE WEEK
+            </h1>
+            <p className="md:text-sm text-xs">
+              Join the party and hit the jackpot today at Funky Coins - where{" "}
+              every bet is a chance to turn up the volume on your winnings!
+            </p>
+            <button className="gap-3 rounded-lg group uppercase px-3 py-2 bg-primary flex items-center justify-center md:justify-start md:text-sm text-xs font-medium sm:w-fit">
+              <p className="uppercase md:text-sm text-xs font-medium">
+                connect wallet
+              </p>
+              <FaChevronCircleRight
+                size={18}
+                className="group-hover:ml-2 transition-all text-black bg-white rounded-full"
+              />
+            </button>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
         dark: "#1a1a1a",
         primary: "#EB0029",
       },
+      backgroundImage: {
+        one: "url('/assets/mainSlider/bgOne.jpeg')",
+        two: "url('/assets/mainSlider/bgTwo.jpeg')",
+        three: "url('/assets/mainSlider/bgThree.jpeg')",
+      },
     },
   },
   plugins: [],
