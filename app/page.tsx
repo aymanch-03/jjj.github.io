21b10b36e9abc086f6ea6e491774a110588226e0
Aymen <Aymen@MacBook-Air.local>
2023-05-23 19:13:45 +0100

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <>
       <div className="md:ml-[90px] sm:ml-[4.5rem] mx-auto">
-        <Originals />
         <MainSlider />
+        <Originals />
         <Statistics />
       </div>
     </>
--- components/MainSlider.tsx
+++ components/MainSlider.tsx
@@ -11,26 +11,26 @@ import { Pagination } from "swiper";
 
 const MainSlider = () => {
   return (
-    <>
+    <div className="sm:px-12 px-5 pb-12 md:pt-[10rem] sm:pt-[8rem] pt-[6rem]">
       <Swiper
-        spaceBetween={30}
+        spaceBetween={40}
         pagination={{
           clickable: true,
         }}
         modules={[Pagination]}
-        className="mySwiper"
+        className="mySwiper w-full h-[25rem]"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide className="bg-[url('../public/assets/bg-1.jpeg')] w-[90%] h-[90%] bg-white text-black p-16">
+          <h1>GAME OF THE WEEK</h1>
+          <p>
+            Join the party and hit the jackpot today at Funky Coins - where
+            every bet is a chance to turn up the volume on your winnings!
+          </p>
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
-    </>
+    </div>
   );
 };
 
